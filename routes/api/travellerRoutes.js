0748f7d8557ed0395446769b47e29d12a4820c74
Stephany <136421961+splacito22@users.noreply.github.com>
2023-11-10 15:09:23 -0800

    working create and delete for travellers

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -26,4 +26,30 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const travellerData = await Traveller.create(req.body);
+    req.status(200).json(travellerData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const travellerData = await Traveller.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!travellerData) {
+      res.status(404).json({ message: "No traveller found with this id!" });
+      return;
+    }
+    res.status(200).json(travellerData);
+  } catch (error) {
+    res.status(500).json(travellerData);
+  }
+});
+
 module.exports = router;
