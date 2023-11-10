16ac2273e88f2fd0b23350126803481dc06a4138
Stephany <136421961+splacito22@users.noreply.github.com>
2023-11-10 14:50:31 -0800

    traveller get by id route

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
-const { Traveller } = require("../../models");
+const { Traveller, Location, Trip } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
     const travellerData = await Traveller.findAll();
+    res.status(200).json(travellerData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const travellerData = await Traveller.findByPk(req.params.id, {
+      include: [{ model: Location, through: Trip, as: "planned_trips" }],
+    });
+
+    if (!travellerData) {
+      res.status(404).json({ message: "No traveller found with this id!" });
+      return;
+    }
     res.status(200).json(travellerData);
   } catch (error) {
     res.status(500).json(error);
